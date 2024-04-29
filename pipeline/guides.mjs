01800df3d2d8cb74fc7cb6f26291a360014605bc
Joey Perrott <josephperrott@gmail.com>
2024-04-29 19:56:42 +0000

    main - 77b373e Revert "refactor(shared-docs): use puppeteer instead of playwright (#1993)" (#2024)

--- pipeline/guides.mjs
+++ pipeline/guides.mjs
@@ -48690,7 +48690,7 @@ var mermaid_exports = {};
 __export(mermaid_exports, {
   processMermaidCodeBlock: () => processMermaidCodeBlock
 });
-import { launch } from "puppeteer-core";
+import { chromium } from "playwright-core";
 function getMermaidScriptTagData() {
   if (mermaidScriptTagData) {
     return mermaidScriptTagData;
@@ -48701,7 +48701,7 @@ function getMermaidScriptTagData() {
 }
 async function processMermaidCodeBlock(token) {
   const diagram = token.code;
-  const browser = await launch({
+  const browser = await chromium.launch({
     headless: true,
     executablePath: import_runfiles.runfiles.resolveWorkspaceRelative(process.env["CHROME_BIN"]),
     args: ["--no-sandbox"]
